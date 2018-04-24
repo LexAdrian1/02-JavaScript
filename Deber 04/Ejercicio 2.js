344b40844cf48a02cada79e9250eec3d10122770
LexAdrian <lexadrian1@gmail.com>
2018-04-24 01:39:05 -0500

    Ejercicio 2

--- Deber 04/Ejercicio 2.js
+++ Deber 04/Ejercicio 2.js
@@ -1,16 +1,4 @@
-/*La Empresa Eléctrica Quito tiene 19 generadores de múltiples tipos.
-Los primeros 4 generan 62 megawatts y los otros 15 generan 124 megawatts
-La Empresa Eléctrica Quito ha decidido contratarlo a usted para diseñar un sistema de "loops" para imprimir en consola
-el total de megawatts generado por cada generador con el siguiente formato:
-- "Generador #1 está prendido, añadiendo 62 MW para un total de 62 MW."
-- "Generador #2 está prendido, añadiendo 62 MW para un total de 124 MW."
-El primer loop es requerido que sea construido con "while" administrando los 4 primeros generadores y el segundo que sea
-construido con un "for" para los 15 generadores restantes.
-Como una funcionalidad adicional quisieran que se puedan activar los generadores pares, impares o todos.
-Es decir, en el ejemplo anterior el formato imprimiría lo siguiente para los generadores impares:
-- "Generador #1 está prendido, añadiendo 62 MW para un total de 62 MW."
-- "Generador #2 está apagado." */
-var text='';
+
 var i=0,j=0;
 var costo=0;
 var condicion='impares';
