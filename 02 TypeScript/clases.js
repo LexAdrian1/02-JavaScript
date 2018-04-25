8e54229489307a62c9c3f9bbf977a73a508c383c
Alexis <lexadrian1@gmail.com>
2018-04-25 13:08:02 -0500

    TypeScript

--- 02 TypeScript/clases.js
+++ 02 TypeScript/clases.js
@@ -1,3 +1,46 @@
 console.log('Hellodaa');
-var hola = 'Hola';
 var adios = 'Adios';
+//Duck typing
+var edad;
+var nombre;
+var casado;
+var fechaNacimiento = new Date();
+var Usuario = /** @class */ (function () {
+    function Usuario(nombre, casado, edad) {
+        this.nombre = nombre;
+        this.casado = casado;
+        this.edad = edad;
+    }
+    return Usuario;
+}());
+var adrian = new Usuario('Adrian', false, 28);
+var UsuarioDos = /** @class */ (function () {
+    function UsuarioDos(nombre, _casado, _edad) {
+        this.nombre = nombre;
+        this._casado = _casado;
+        this._edad = _edad;
+    }
+    Object.defineProperty(UsuarioDos.prototype, "casado", {
+        get: function () {
+            return this._casado;
+        },
+        set: function (casado) {
+            this._casado = casado;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    UsuarioDos.prototype.imprimirUsuario = function (saludo) {
+        //  Template Strings
+        return saludo + ". Minombre es " + this.nombre + ",estoy casado " + this.casado + ", mi edad es " + this._edad;
+    };
+    return UsuarioDos;
+}());
+var lexxarDos = new UsuarioDos('adrian', false, 20);
+console.log(lexxarDos);
+console.log(lexxarDos.imprimirUsuario('Hola soy koku'));
+var carla = {
+    nombre: 'Adrian',
+    casado: false,
+    edad: 28
+};
--- 02 TypeScript/clases.ts
+++ 02 TypeScript/clases.ts
@@ -1,6 +1,49 @@
 console.log('Hellodaa');
-let hola = 'Hola';
 const adios = 'Adios';
+//Duck typing
 
+let edad: number;
+let nombre: string;
+let casado: boolean;
+let fechaNacimiento: Date = new Date();
 
+class Usuario {
+    nombre: string;
+    private casado: boolean;
+    protected edad: number;
+    constructor(nombre: string, casado: boolean, edad: number) {
+        this.nombre = nombre;
+        this.casado = casado;
+        this.edad = edad;
+    }
+}
+let adrian: Usuario = new Usuario('Adrian', false, 28);
 
+class UsuarioDos {
+    constructor(public nombre:string,private _casado:boolean,protected _edad:number){
+    }
+    get casado(){
+        return this._casado;
+    }
+    set casado(casado: boolean){
+        this._casado = casado;
+    }
+    public imprimirUsuario(saludo: string): string{
+        //  Template Strings
+        return `${saludo}. Minombre es ${this.nombre},estoy casado ${this.casado}, mi edad es ${this._edad}`;
+    }
+}
+let lexxarDos = new UsuarioDos( 'adrian',false,20);
+console.log(lexxarDos);
+console.log(lexxarDos.imprimirUsuario('Hola soy koku'));
+let carla: UsuarioTres={
+    nombre: 'Adrian',
+    casado: false,
+    edad: 28
+};
+interface  UsuarioTres {
+    nombre: string;
+    casado?: boolean; //Opcional
+    edad: number;
+    imprimir?(saludo: string):string;
+}
